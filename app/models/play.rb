97a7cf1bcc78c030de013459ce2c4b545b3ed9de
Bill Walker <bwalker@mozilla.com>
2014-06-03 09:46:16 -0700

    add another has_many link

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,5 +1,6 @@
 class Play < ActiveRecord::Base
-  belongs_to :playwright
-  has_many :roles
-  attr_accessible :notes, :premiere_year, :title, :playwright_id
+	belongs_to :playwright
+	has_many :runs
+	has_many :roles
+	attr_accessible :notes, :premiere_year, :title, :playwright_id
 end
