27d251e9f0c02cf79a7a15580fbf3e30c97c60e3
Bill Walker <bwalker@mozilla.com>
2014-06-25 10:06:30 -0700

    add some has_many links

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ActiveRecord::Base
+  has_many :performances
   attr_accessible :name
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,7 @@
 class Role < ActiveRecord::Base
   belongs_to :play
   attr_accessible :name, :play_id
+  has_many :performances
 
   def play_and_name
   	"#{play.title} > #{name}"
